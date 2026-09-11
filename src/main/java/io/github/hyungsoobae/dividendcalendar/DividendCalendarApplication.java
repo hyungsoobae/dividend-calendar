7fa3c849fd09d20241a0eafa95f3943c8af2fe70
hyungsoobae <hs950823@gmail.com>
2026-09-11 17:09:39 +0900

    chore: Spring Boot 4.1 프로젝트 생성 (Gradle Kotlin DSL, Java 21)

---
+++ src/main/java/io/github/hyungsoobae/dividendcalendar/DividendCalendarApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyungsoobae.dividendcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividendCalendarApplication.class, args);
+	}
+
+}
